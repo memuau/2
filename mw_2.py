b38e22b02a55092b0dabf936efa23178c4e027ca
Maciej <monocyklon@gmail.com>
2021-08-03 17:10:07 +0200

    problem solved

--- mw_2.py
+++ mw_2.py
@@ -1,4 +1,7 @@
-similar_chars = {'A': '4', 'B': 'DO80', 'C': '', 'D': 'ODB0', 'E': 'F', 'F': 'E', 'G': '', 'H': '', 
+from difflib import SequenceMatcher
+import pandas as pd
+
+similar_chars = {'A': '4', 'B': 'DO80', 'C': '', 'D': 'O8B0', 'E': 'F', 'F': 'E', 'G': '', 'H': '', 
 'I': '1L', 'J': '', 'K': '', 'L': '1J', 'M': 'NW', 'N': 'MW', 'O': '0UD', 'P': 'R', 'R': 'P', 'S': '5', 
 'T': '', 'U': 'OD', 'V': 'WNMU', 'W': 'MN', 'X': 'Y', 'Y': 'X', 'Z': '2', '0': 'OUD', '1': 'IL', '2': 
 'Z', '3': '', '4': 'A','5': 'S', '6': '', '7': '', '8': 'BO0', '9': ''} ## kluczem jest znak, a wartością znaki wyglądające podobnie
@@ -14,30 +17,42 @@ unique_plates = ['W1SMART', 'NUDE202', 'NODE202', 'NDDE202', 'NDDE202J', 'WDDE20
  'NOBE20E', 'NDDE272'] #wczytane z zadania 1 na potrzeby badawcze
 
 
-def similarity(a: str, b: str):
+def similarity_2(a: str, b: str, similar_char_tolerance: float):
+    match = SequenceMatcher(None, a, b).find_longest_match(0, len(a), 0, len(b)) # na początku znajduję najdłuższy identyczny substring,
+    # robię to po to, aby "wygładzić" skutki braku jednego znaku w środku stringa, które w poprzednim algorytmie mocno zaniżały wynik
+    new_a = a[:match.a] + a[match.a+match.size:]
+    new_b = b[:match.b] + b[match.b+match.size:]
+    #print(new_a)
+    #print(new_b)
+    #od tego miejsca funkcja ma taki sam przebieg, ale rozważa już tylko stringi pomniejszone o największy wspólny substring
     big_counter = 0 # na końcu zostanie podzielony przez liczbę znaków aby otrzymać średnie podobieństwo
-    pairs = list(zip(a,b)) # z obu napisów tworzę pary znaków występujących na tych samych miejscach
+    pairs = list(zip(new_a,new_b)) # z obu napisów tworzę pary znaków występujących na tych samych miejscach
     for pair in pairs:
         small_counter = 0 
         if pair[0] == pair[1]:
             small_counter +=1 # znaki są takie same
         elif pair[1] in similar_chars[pair[0]]:
-            small_counter +=0.75 # tę wartość można jeszcze dostroić
+            small_counter += similar_char_tolerance # tę wartość można jeszcze dostroić
         big_counter += small_counter 
         small_counter = 0
-    ratio = big_counter/len(pairs) #średnie podobieństwo
-    return ratio - abs(len(a) - len(b))/max(len(a), len(b)) #jeśli napisy były różnych długości, to trzeba zmniejszyć ich podobieństwo 
+    return ((match.size * (max(len(a), len(b)) - abs(match.a - match.b))/max(len(a), len(b))) + big_counter )/max(len(a), len(b))
 
+
+dflist = []
 for plate in unique_plates :
-    print(f"NODE202 and {plate} similarity {similarity(unique_plates[2], plate)}")
+    mylist = []
+    #print(f"NODE202 and {plate} {similarity_2(unique_plates[2], plate, 0.5)}")
+    mylist.append(f"NODE202 and {plate}")
+    mylist.append(similarity_2(unique_plates[2], plate, 0.4))
+    mylist.append(similarity_2(unique_plates[2], plate, 0.5))
+    mylist.append(similarity_2(unique_plates[2], plate, 0.6))
+    mylist.append(similarity_2(unique_plates[2], plate, 0.7))
+    mylist.append(similarity_2(unique_plates[2], plate, 0.8))
+    mylist.append(similarity_2(unique_plates[2], plate, 0.9))
+    dflist.append(mylist)
 
-"""
-PROBLEM DO ROZWIĄZANIA:
-DLA NAPISÓW 'NODE202' ORAZ 'NDE202' PODOBIEŃSTWO WYCHODZI BARDZO NISKIE - OKOŁO 0,15
-TO DLATEGO, ŻE ZNAKI SIĘ ROZJECHAŁY PRZEZ TO, ŻE BRAKUJACY/DODATKOWY ZNAK NIE JEST NA KOŃCU, TYLKO W ŚRODKU
 
-CO SPRÓBUJĘ ZROBIĆ:
-FUNKCJA BĘDZIE ZNAJDOWAĆ NAJDŁUŻSZY WSPÓLNY PODCIĄG DLA OBU NAPISÓW
-NASTĘPNIE BĘDZIE WSTAWIAĆ 'G' PRZED LUB PO W/W PODCIĄGU (G, DLATEGO, ŻE NIE MA ZNAKÓW PODOBNYCH DO G W SŁOWNIKU similar_chars)
-DLA KAŻDEGO WSTAWIENIA 'G' FUNKCJA POLICZY PODOBIEŃSTWO, A OSTATECZNIE ZWRÓCI NAJWYŻSZY WYNIK
-"""
+out = pd.DataFrame(dflist, columns=["Porównywane napisy", "0,4", "0,5", "0,6", "0,7", "0,8", "0,9"])
+out.to_excel("tolerance_0_4_to_0_9.xlsx")
+
+#wygenerowałem excela z danymi, które posłużą mi do wybrania optymalnej wartości współczynnika 'similar_char_tolerance'
